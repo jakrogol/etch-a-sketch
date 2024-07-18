40e3b13e259e6b86ea006f3d9698581906fdc127
Jacob Krogol <jacob@krogol.com>
2024-07-18 12:29:55 -0400

    Add HTML elements, basic styles, and JS to draw a grid of squares with a mouseover effect.

---
+++ etch-a-sketch.js
@@ -0,0 +1,28 @@
+const gridContainer = document.getElementById('grid-container');
+const restart = document.getElementById('restart');
+
+restart.addEventListener('click', () => {
+  let columns = 0;
+  while (columns < 1 || columns > 100) {
+    columns = prompt("Please enter a positive number up to 100");
+  }
+  drawGrid(columns);
+});
+
+function drawGrid(columns) {
+  gridContainer.innerHTML = '';
+  let dimension = 700 / columns;
+  for (i = 1; i <= columns**2; i++) {
+    let square = document.createElement('div');
+    square.style.width = `${dimension}px`;
+    square.style.height = `${dimension}px`;
+    square.classList.add('grid-square');
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'lightblue';
+    });
+    square.addEventListener('mouseout', () => {
+      square.style.backgroundColor = '';
+    })
+    gridContainer.appendChild(square);
+  }
+}
